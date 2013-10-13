d4ad2cb8db6d9ae97c3c076c0f7cb82b291b352a
Leonardo Cardoso <ltc270486@hotmail.com>
2013-10-13 19:26:11 -0300

    periodo inicial

--- app/models/hospede.rb
+++ app/models/hospede.rb
@@ -3,5 +3,16 @@ class Hospede < ActiveRecord::Base
   def endereco
     "#{logradouro.split(" ").collect { |word| word.capitalize! }.join(' ')}, #{numero.capitalize!} #{complemento.capitalize!}"
   end
+  def name=(name)
+    @name = name.collect { |char| char.capitalize }.join
+  end
+  def cpf=(cpf)
+    @cpf = sanitiza cpf
+  end
+
+  private
+  def sanitiza(cpf)
+    cpf.each_chr { |char| char.to_i.to_s }.join
+  end
 
 end
